a69eb68b8656acd733b4fd38f714ee2ec0da9d7c
GepengCn <214764500@qq.com>
2018-10-31 14:18:10 +0800

    sqlserver支持

---
+++ src/plugins/tlim/src/database/tlim_sqlserver.sql
@@ -0,0 +1,150 @@
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ofchat')
+            and   type = 'U')
+   drop table ofchat
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ofmessage')
+            and   type = 'U')
+   drop table ofmessage
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ofpubact')
+            and   type = 'U')
+   drop table ofpubact
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ofsession')
+            and   type = 'U')
+   drop table ofsession
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ofstatus')
+            and   type = 'U')
+   drop table ofstatus
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ofstyle')
+            and   type = 'U')
+   drop table ofstyle
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ofsubscriber')
+            and   type = 'U')
+   drop table ofsubscriber
+go
+
+/*==============================================================*/
+/* Table: ofchat                                                */
+/*==============================================================*/
+create table ofchat (
+   chat_id              varchar(64)          not null,
+   chat_name            varchar(64)          null,
+   chat_user            varchar(64)          null,
+   chat_other           varchar(64)          null,
+   chat_create_time     varchar(64)          null,
+   chat_modify_time     varchar(64)          null,
+   chat_pic             varchar(64)          null,
+   constraint PK_OFCHAT primary key nonclustered (chat_id)
+)
+go
+
+/*==============================================================*/
+/* Table: ofmessage                                             */
+/*==============================================================*/
+create table ofmessage (
+   id_                  int                  not null,
+   msg_id               varchar(64)          null,
+   msg_type             varchar(20)          null,
+   msg_from             varchar(64)          null,
+   msg_to               varchar(64)          null,
+   msg_time             varchar(20)          null,
+   body                 varchar(3920)        null,
+   session_id           varchar(64)          null,
+   constraint PK_OFMESSAGE primary key nonclustered (id_)
+)
+go
+
+/*==============================================================*/
+/* Table: ofpubact                                              */
+/*==============================================================*/
+create table ofpubact (
+   id_                  int                  not null,
+   title                varchar(256)         null,
+   content              varchar(3920)        null,
+   user_id              varchar(64)          null,
+   ts                   varchar(64)          null,
+   session_id           varchar(64)          null,
+   constraint PK_OFPUBACT primary key nonclustered (id_)
+)
+go
+
+/*==============================================================*/
+/* Table: ofsession                                             */
+/*==============================================================*/
+create table ofsession (
+   session_id           varchar(64)          not null,
+   session_type         int                  null,
+   session_name         varchar(256)         null,
+   session_create_time  varchar(20)          null,
+   session_modify_time  varchar(20)          null,
+   session_delete_time  varchar(20)          null,
+   session_valid        int                  null,
+   "session_user"       varchar(64)          null,
+   session_pic          varchar(256)         null,
+   constraint PK_OFSESSION primary key nonclustered (session_id)
+)
+go
+
+/*==============================================================*/
+/* Table: ofstatus                                              */
+/*==============================================================*/
+create table ofstatus (
+   id_                  int                  not null,
+   msg_id               varchar(64)          null,
+   msg_to               varchar(64)          null,
+   msg_type             varchar(64)          null,
+   status               int                  null,
+   session_id           varchar(64)          null,
+   constraint PK_OFSTATUS primary key nonclustered (id_)
+)
+go
+
+/*==============================================================*/
+/* Table: ofstyle                                               */
+/*==============================================================*/
+create table ofstyle (
+   style_id             int                  not null,
+   style_name           varchar(64)          null,
+   style_value          int                  null,
+   user_id              varchar(64)          null,
+   constraint PK_OFSTYLE primary key nonclustered (style_id)
+)
+go
+
+/*==============================================================*/
+/* Table: ofsubscriber                                          */
+/*==============================================================*/
+create table ofsubscriber (
+   user_id              varchar(64)          not null,
+   user_name            varchar(256)         null,
+   acct_login           varchar(64)          null,
+   pic                  varchar(128)         null,
+   session_id           varchar(64)          not null,
+   ts                   varchar(20)          null,
+   constraint PK_OFSUBSCRIBER primary key nonclustered (user_id, session_id)
+)
+go
--- target/openfire/plugins/tlim/database/tlim_oracle.sql
+++ target/openfire/plugins/tlim/database/tlim_oracle.sql
@@ -1,115 +1,120 @@
-drop table "ofchat" cascade constraints;
+drop table ofchat cascade constraints;
 
-drop table "ofmessage" cascade constraints;
+drop table ofmessage cascade constraints;
 
-drop table "ofpubact" cascade constraints;
+drop table ofpubact cascade constraints;
 
-drop table "ofsession" cascade constraints;
+drop table ofsession cascade constraints;
 
-drop table "ofstatus" cascade constraints;
+drop table ofstatus cascade constraints;
 
-drop table "ofstyle" cascade constraints;
+drop table ofstyle cascade constraints;
 
-drop table "ofsubscriber" cascade constraints;
+drop table ofsubscriber cascade constraints;
 
 /*==============================================================*/
-/* Table: "ofchat"                                              */
+/* Table: ofchat                                              */
 /*==============================================================*/
-create table "ofchat"
+create table ofchat
 (
-   "chat_id"            VARCHAR2(64)         not null,
-   "chat_name"          VARCHAR2(64),
-   "chat_user"          VARCHAR2(64),
-   "chat_other"         VARCHAR2(64),
-   "chat_create_time"   VARCHAR2(64),
-   "chat_modify_time"   VARCHAR2(64),
-   "chat_pic"           VARCHAR2(64),
-   constraint PK_OFCHAT primary key ("chat_id")
+   chat_id            VARCHAR2(64)         not null,
+   chat_name          VARCHAR2(64),
+   chat_user          VARCHAR2(64),
+   chat_other         VARCHAR2(64),
+   chat_create_time   VARCHAR2(64),
+   chat_modify_time   VARCHAR2(64),
+   chat_pic           VARCHAR2(64),
+   constraint PK_OFCHAT primary key (chat_id)
 );
 
 /*==============================================================*/
-/* Table: "ofmessage"                                           */
+/* Table: ofmessage                                           */
 /*==============================================================*/
-create table "ofmessage"
+create table ofmessage
 (
-   "id_"                INTEGER              not null,
-   "msg_id"             VARCHAR2(64),
-   "msg_type"           VARCHAR2(20),
-   "msg_from"           VARCHAR2(64),
-   "msg_to"             VARCHAR2(64),
-   "msg_time"           VARCHAR2(20),
-   "body"               VARCHAR2(3920),
-   "session_id"         VARCHAR2(64),
-   constraint PK_OFMESSAGE primary key ("id_")
+   id_                INTEGER              not null,
+   msg_id             VARCHAR2(64),
+   msg_type           VARCHAR2(20),
+   msg_from           VARCHAR2(64),
+   msg_to             VARCHAR2(64),
+   msg_time           VARCHAR2(20),
+   body               VARCHAR2(3920),
+   session_id         VARCHAR2(64),
+   constraint PK_OFMESSAGE primary key (id_)
 );
 
 /*==============================================================*/
-/* Table: "ofpubact"                                            */
+/* Table: ofpubact                                            */
 /*==============================================================*/
-create table "ofpubact"
+create table ofpubact
 (
-   "id_"                INTEGER              not null,
-   "title"              VARCHAR2(256),
-   "content"            VARCHAR2(3920),
-   "user_id"            VARCHAR2(64),
-   "ts"                 VARCHAR2(64),
-   "session_id"         VARCHAR2(64),
-   constraint PK_OFPUBACT primary key ("id_")
+   id_                INTEGER              not null,
+   title              VARCHAR2(256),
+   content            VARCHAR2(3920),
+   user_id            VARCHAR2(64),
+   ts                 VARCHAR2(64),
+   session_id         VARCHAR2(64),
+   constraint PK_OFPUBACT primary key (id_)
 );
 
 /*==============================================================*/
-/* Table: "ofsession"                                           */
+/* Table: ofsession                                           */
 /*==============================================================*/
-create table "ofsession"
+create table ofsession
 (
-   "session_id"         VARCHAR2(64)         not null,
-   "session_type"       INTEGER,
-   "session_name"       VARCHAR2(256),
-   "session_create_time" VARCHAR2(20),
-   "session_modify_time" VARCHAR2(20),
-   "session_delete_time" VARCHAR2(20),
-   "session_valid"      INTEGER,
-   "session_user"       VARCHAR2(64),
-   "session_pic"        VARCHAR2(256),
-   constraint PK_OFSESSION primary key ("session_id")
+   session_id         VARCHAR2(64)         not null,
+   session_type       INTEGER,
+   session_name       VARCHAR2(256),
+   session_create_time VARCHAR2(20),
+   session_modify_time VARCHAR2(20),
+   session_delete_time VARCHAR2(20),
+   session_valid      INTEGER,
+   session_user       VARCHAR2(64),
+   session_pic        VARCHAR2(256),
+   constraint PK_OFSESSION primary key (session_id)
 );
 
 /*==============================================================*/
-/* Table: "ofstatus"                                            */
+/* Table: ofstatus                                            */
 /*==============================================================*/
-create table "ofstatus"
+create table ofstatus
 (
-   "id_"                INTEGER              not null,
-   "msg_id"             VARCHAR2(64),
-   "msg_to"             VARCHAR2(64),
-   "msg_type"           VARCHAR2(64),
-   "status"             INTEGER,
-   "session_id"         VARCHAR2(64),
-   constraint PK_OFSTATUS primary key ("id_")
+   id_                INTEGER              not null,
+   msg_id             VARCHAR2(64),
+   msg_to             VARCHAR2(64),
+   msg_type           VARCHAR2(64),
+   status             INTEGER,
+   session_id         VARCHAR2(64),
+   constraint PK_OFSTATUS primary key (id_)
 );
 
 /*==============================================================*/
-/* Table: "ofstyle"                                             */
+/* Table: ofstyle                                             */
 /*==============================================================*/
-create table "ofstyle"
+create table ofstyle
 (
-   "style_id"           INTEGER              not null,
-   "style_name"         VARCHAR2(64),
-   "style_value"        INTEGER,
-   "user_id"            VARCHAR2(64),
-   constraint PK_OFSTYLE primary key ("style_id")
+   style_id           INTEGER              not null,
+   style_name         VARCHAR2(64),
+   style_value        INTEGER,
+   user_id            VARCHAR2(64),
+   constraint PK_OFSTYLE primary key (style_id)
 );
 
 /*==============================================================*/
-/* Table: "ofsubscriber"                                        */
+/* Table: ofsubscriber                                        */
 /*==============================================================*/
-create table "ofsubscriber"
+create table ofsubscriber
 (
-   "user_id"            VARCHAR2(64)         not null,
-   "user_name"          VARCHAR2(256),
-   "acct_login"         VARCHAR2(64),
-   "pic"                VARCHAR2(128),
-   "session_id"         VARCHAR2(64)         not null,
-   "ts"                 VARCHAR2(20),
-   constraint PK_OFSUBSCRIBER primary key ("user_id", "session_id")
+   user_id            VARCHAR2(64)         not null,
+   user_name          VARCHAR2(256),
+   acct_login         VARCHAR2(64),
+   pic                VARCHAR2(128),
+   session_id         VARCHAR2(64)         not null,
+   ts                 VARCHAR2(20),
+   constraint PK_OFSUBSCRIBER primary key (user_id, session_id)
 );
+
+INSERT INTO ofVersion(name,version) values('tlim',1);
+
+INSERT INTO ofProperty(name,propValue,encrypted) values('route.all-resources','true',0);
+
